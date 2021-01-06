ac0f15106e5196b681494ace840626eaac031a20
Cole Bergmann <colebergmann@ucsb.edu>
2021-01-05 22:33:31 -0800

    handles reservations in any order, and forced calendar checks to use pacific time

--- reservation.py
+++ reservation.py
@@ -3,6 +3,7 @@ import requests
 from ics import Calendar, Event
 import arrow as arrow
 from config import config_dict
+import traceback
 
 ########
 # Returns true if we have a reservation checking in today, false otherwise
@@ -13,18 +14,23 @@ def reservation_today():
     try:
         calendar_url = "https://colebergmann.com/sonos/test.ics"
         c = Calendar(requests.get(config_dict["cal_url"]).text)
-        e = list(c.timeline)[0] # first event in the list
-        tprint("Event found for {} (starting {})".format(e.begin.format('YYYY-MM-DD'), e.begin.humanize()))
-        if e.begin.format('YYYY-MM-DD') == arrow.utcnow().format('YYYY-MM-DD'):
-            tprint("We have a reservation starting today, returning True (from reservation_today)")
-            return True
-        else:
-            tprint("We DO NOT have a reservation starting today, returning False (from reservation_today)")
-            return False
+        events = list(c.timeline)
+        for i in range(0, len(events)):
+            e = events[i] # first event in the list
+            tprint("Event found for {} (starting {})".format(e.begin.format('YYYY-MM-DD'), e.begin.humanize()))
+            if e.begin.format('YYYY-MM-DD') == arrow.utcnow().to('US/Pacific').format('YYYY-MM-DD'):
+                tprint("We have a reservation starting today, returning True (from reservation_today)")
+                return True
+        # if we did not find a valid event, return False
+        tprint("We DO NOT have a reservation starting today, returning False (from reservation_today)")
+        return False
     except Exception as e:
         tprint("[Reservation] ERROR: An error occured while fetching the latest reservation. Assuming we don't have a reservation...")
         print(e)
+        print(traceback.format_exc())
         return False
 
 def tprint(s):
     print("\033[94m" + arrow.utcnow().to('US/Pacific').format('MM/DD/YYYY HH:mm') + " [Reservation]\033[0m " + s)
+
+reservation_today()
